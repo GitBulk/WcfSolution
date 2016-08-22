969a28029caa112cae0598cea860d40a63ebdc0c
gitbulk <giacatiensinh@hotmail.com>
2016-08-22 14:49:16 +0700

    init service host

---
+++ Service.Host/Bootstrapper.cs
@@ -0,0 +1,28 @@
+﻿using Autofac;
+using Business.Services;
+using Business.Services.Contracts;
+using Data.Core.Infrastructures;
+using Data.Core.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Service.Host
+{
+    public static class Bootstrapper
+    {
+        public static IContainer BuildContainer()
+        {
+            var builder = new ContainerBuilder();
+            builder.RegisterType<BlogService>().As<IBlogService>();
+            builder.RegisterType<ArticleService>().As<IArticleService>();
+
+            builder.RegisterType<UnitOfWork>().As<IUnitOfWork>();
+            builder.RegisterType<DbFactory>().As<IDbFactory>();
+            builder.RegisterType<ArticleRepository>().As<IArticleRepository>();
+            builder.RegisterType<BlogRepository>().As<IBlogRepository>();
+            return builder.Build();
+        }
+    }
+}
---
+++ Service.Host/readme.txt
@@ -0,0 +1,4 @@
+﻿install Autofac, Autofac.Wcf
+add references to Business.Services, Data.Core projects and System.ServiceModel assembly
+
+Empty ASP.NET Web application (Hosts WCF Services)
